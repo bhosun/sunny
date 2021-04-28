1497c195e2972299514acbe7604e16417d0b5a49
Manuel Solomon <bhorsun@yahoo.com>
2021-04-27 22:39:02 -0800

    no of user's feature

--- Search/src/app.js
+++ Search/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const Pass = require("./models/pass.model");
 const User = require("./models/user.model");
 const app = express();
@@ -9,6 +10,21 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.json({ msg: "search Page!"});
 });
+
+// GET NO OF USERS ON THE APP
+app.get("/users", async (req, res) => {
+    try {
+        fetch("http://localhost:3001/users")
+        .then(res => res.json())
+        .then(json => res.send(` there are ${json.length} users on this app`));
+    } catch(e) {
+        res.status(500).json(e);
+    }
+});
+
+// GET YORUBA USERS ON THE APP
+
+// VERIFY PASSWORD use user and password
 
 
 module.exports = app;
