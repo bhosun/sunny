3439f98229ab53c9d07215b80d406ae62985116f
Manuel Solomon <bhorsun@yahoo.com>
2021-04-27 23:44:15 -0800

    statistics of specifics feature completed

--- Search/src/app.js
+++ Search/src/app.js
@@ -27,7 +27,17 @@ app.get("/yorubausers", async (req, res) => {
     try {
         fetch("http://localhost:3001/users")
         .then(res => res.json())
-        .then(json => res.send(` there are ${json.length} users on this app`));
+        .then(json => {
+            let count = 0;
+            for(let i = 0; i < json.length; i++) {
+                if (json[i].name.slice(0,3) === "Ola") {
+                    count++;
+                } else {
+                    continue;
+                }
+            }
+            res.send(`There are ${count} yoruba users on this app`);
+        });
     } catch(e) {
         res.status(500).json(e);
     }
